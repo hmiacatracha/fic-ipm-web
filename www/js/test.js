4be1f83847a204d6ceabd04f06dbc89a46df744e
Diego Trabazo Sardón <dtrabazo@gmail.com>
2015-12-15 12:24:59 +0100

    Added code to show secondary section. TODO: button doesn't not appear with it's image anymore
    Relevant info:
    https://stackoverflow.com/questions/6246273/how-to-change-the-display-of-a-div-using-javascript
    https://stackoverflow.com/questions/6242976/javascript-hide-show-element
    https://stackoverflow.com/questions/2738920/background-image-for-input-type-button
    https://stackoverflow.com/questions/2920076/html-css-how-to-add-image-icon-to-input-type-button

---
+++ www/js/test.js
@@ -0,0 +1,9 @@
+/* 
+ * Javascritp code to handle requested actions. 
+ */
+
+function show_seconday_section () {
+  secondary_section = document.getElementById("seccion_secundaria");
+  seccion_secundaria.style.display = 'block';
+  
+}
