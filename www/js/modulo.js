e81e3bdcdee459a42a460a91fba734efc6cd2b98
Heidy Mabel Izaguirre <hmiacatracha@gmail.com>
2015-12-16 13:58:27 +0100

    Modificacion del javaScrip para que funcione añadir elementos a la lista

--- www/js/modulo.js
+++ www/js/modulo.js
@@ -1,5 +1,8 @@
 var moduloModule = (function () {
 
+	var elemento = 7; 
+	var datos = [];
+
 	var principal = {
 		nuevoB:document.getElementById("nuevo_btn"),
 		spp:document.getElementById("seccion_principal_p"),
@@ -9,11 +12,12 @@ var moduloModule = (function () {
 	var principal_d = {
 		listaD: document.getElementById("lista_detalle"),
 		spd:document.getElementById("seccion_principal_d"),
-		nombre:document.getElementById("detallePedido"),		
+		detalle:document.getElementById("detallePedido"),		
 
 	};
 
 	var secundaria = {
+		pedidoF:document.getElementsByClassName("pedido"),
 		ssp:document.getElementById("seccion_secundaria"),
 		nombreI:document.getElementById("nombreInput"),
 		fechaI:document.getElementById("fechaInput"),
@@ -37,30 +41,88 @@ var moduloModule = (function () {
 		console.log("doAceptar");	
 		var nombre = secundaria.nombreI.value;	
 		var fecha = secundaria.fechaI.value;
-			
-		if (nombre == "" && fecha == "") {
-			alert ("Introduzca los datos del formulario");
-		} else if (nombre == "") {
-			alert("Introduzca el nombre");
-		} else if (fecha == "") {
-			alert("Introduzca la fecha");
-		}else {
+
+		if (nombre != ""){
+			anadirDatos(nombre, fecha);
 			//añadir los datos
 			doLimpiar();
+
+		} else if (nombre === "") {
+			alert("Introduzca el nombre");
+			secundaria.nombreI.focus();
+			secundaria.ssp.style.display = 'block';
+		}				
+	};
+
+	function anadirDatos(nombre, fecha){
+		
+		var lu = principal.listaP;
+		var li = document.createElement('li');
+		li.id = "elemento-"+ elemento.toString();
+		var a = document.createElement('a');
+		var spam = document.createElement("spam");
+
+		
+		var txt_nombre = document.createTextNode(nombre);
+		a.href ="details.html?nombre=" + nombre + "&fecha="+ fecha;
+		a.appendChild(txt_nombre);		
+		var txt_fecha = document.createTextNode(fecha);		
+		spam.appendChild(txt_fecha);
+
+		li.appendChild(a);
+		li.appendChild(spam);
+		lu.appendChild(li);
+		elemento++;
+	};
+
+	function cargarDatos(){
+		for(var i=7, t=localStorage.length; i < t; i++) {
+		    key = localStorage.key(i);
+		    //var paramarr = key.split ("&");
+		    //var nombre = paramarr
+		    anadirDatos(nombre, fecha);
+		    localStorage.setItem(elemento, nombre +"&" + fecha);			
+		}
+	};
+
+	function cargarDatosDetalles(){	
+		var detalle_pedido = "Detalle del pedido ";
+		var paramstr = location.search.substring(1,location.search.length);
+		var paramarr = paramstr.split ("&");
+		var params = {};
+
+		for ( var i = 0; i < paramarr.length; i++) {
+			var tmparr = paramarr[i].split("=");
+			params[tmparr[0]] = tmparr[1];
+		    }
+
+		if (params['nombre']) {
+			detalle_pedido = detalle_pedido + params['nombre'];
+			console.log('El valor del parámetro nombre es: '+params['nombre']);
+
+		} if (params['fecha']){
+			detalle_pedido = detalle_pedido + params['fecha'];
+			console.log('El valor del parámetro nombre es: '+params['fecha']);
+			
+		}else {
+			console.log('No se envió el parámetro variable');
 		}
 
-				
+		principal_d.detalle.value = detalle_pedido; 
+
 	};
+
 
 	function doLimpiar(){
 		console.log("doLimpiar");
+		pedidoF.reset();
 		secundaria.ssp.style.display="none";
-		secundaria.fechaI.value = "";
-		secundaria.nombreI.value = "";		
+		/*principal.listaP.style.display='block';*/				
 	};
 
 	function doNuevo () {
 		console.log("doNuevo");	
+		/*principal.listaP.style.display="none";*/
   		secundaria.ssp.style.display = 'block';
 	};
 
@@ -79,3 +141,7 @@ moduloModule.init();
 
 
 
+
+
+
+	
